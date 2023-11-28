8e1307f0a17330ee1b7fb2d7872fdeda8a3bc7fa
Aathifah Dewantari <147472868+thifahdewantari@users.noreply.github.com>
2023-11-28 12:39:08 +0700

    Pertemuan tanggal 28 Nop 2023 2

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,28 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master')
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Data Pegawai</h3>
+@section('title', 'Data Pegawai')
 
-	<a href="/pegawai"> Kembali</a>
+@section('konten')
+    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+    <h3>Data Pegawai</h3>
 
-	<br/>
-	<br/>
+    <a href="/pegawai"> Kembali</a>
 
-	<form action="/pegawai/store" method="post">
-		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
+    <br />
+    <br />
 
-
-
-</body>
-</html>
+    <form action="/pegawai/store" method="post" class="form-horizontal">
+        {{ csrf_field() }}
+        <div class="form-group row">
+            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
+            <div class="col-xs-9">
+            <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+            </div>
+        Nama <input type="text" name="nama"> <br />
+        Jabatan <input type="text" name="jabatan"> <br />
+        Umur <input type="number" name="umur"> <br />
+        Alamat
+        <textarea name="alamat"></textarea> <br />
+        <input type="submit" value="Simpan Data">
+    </form>
+@endsection
